79745c4856482b912b881f51f0cf43eb7c0d20b7
Afton Downey and Bri Popson <aaike001@gmail.com>
2016-02-08 14:46:19 -0800

    begin building volume and finalPrice methods

--- parcels.php
+++ parcels.php
@@ -7,6 +7,9 @@ class Parcel
     private $length;
     private $weight;
 
+
+
+
     function __construct($height, $length, $width, $weight)
     {
         $this->height = $height;
@@ -52,8 +55,50 @@ class Parcel
 
     function getWeight()
     {
-      return $this->wyeight;
+      return $this->weight;
     }
 
+    function volume($height, $width, $length)
+    {
+        return $this->height * $this->width * $this->length;
+    }
+
+    function totalPrice($volume, $weight)
+    {
+        return ($this->volume * 0.13) + ($this->weight * .25);
+    }
 }
+
+
+
+// $parcels_total = array();
+// foreach ($parcels as $parcel) {
+//     $parcel_height = $parcel->getHeight();
+//     $parcel_width = $parcel->getWidth();
+//     $parcel_length = $parcel->getLength();
+//       if ($car_price < $_GET["price"] && $car_mileage < $_GET["miles"]) {
+//           array_push($cars_matching_search, $car);
+//       }
+//   }
+
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pricing</title>
+</head>
+<body>
+    <h1>Here is yo shipping cost</h1>
+    <?php
+        $object = new Parcel($_GET["height"], $_GET["length"], $_GET["width"], $_GET["weight"]);
+        $object_volume = $object->volume();
+        $object_weight = $object->getWeight();
+        $finalPrice = $object->totalPrice();
+    ?>
+    <p>Volume of package: <?php echo $object_volume; ?></p>
+    <p>Weight: <?php echo $object_weight; ?></p>
+    <p>Total: <?php echo $finalPrice; ?></p>
+</body>
+</html>
